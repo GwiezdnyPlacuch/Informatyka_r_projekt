ce6961be4d95e0b35c117be6b9c6938dabd17d7c
GwiezdnyPlacuch <gwiezdnyplacuch@gmail.com>
2022-06-12 17:43:17 +0200

    final final touches

--- Informatyka_r_projekt/Zadania.h
+++ Informatyka_r_projekt/Zadania.h
@@ -16,59 +16,6 @@ public:
     bool are_anagrams(wxString str1, wxString str2);
     bool are_palindrome(wxString str1);
 
-    int ONP(wxString r) {
-
-        std::string wyrazenie = r.ToStdString();
-        std::string operatory = "/+-* ";
-
-        if (wyrazenie.length() % 2==0) return 0;
-
-        for (auto c : wyrazenie) {
-            if (!(std::isdigit(c)) && (operatory.find(c)== std::string::npos))
-                return 0;
-        }
-
-        std::stack<int> stos;
-        int n = wyrazenie.length();
-
-        
-            for (int i = 0; i < n; i++) {
-                int cyfra = (int)wyrazenie.at(i);
-
-                if (isdigit(cyfra)) {
-                    cyfra -= 48;
-                    stos.push(cyfra);
-                }
-                else if (isspace(cyfra)) { continue; }
-                else {
-                    int x, y;
-                    if (stos.size() <= 1) return 0;
-                    x = stos.top();
-                    stos.pop();
-                    y = stos.top();
-                    stos.pop();
-                    
-                        
-                    switch (wyrazenie.at(i))
-                    {
-                    case '-':
-                        stos.push(y - x);
-                        break;
-                    case '+':
-                        stos.push(y + x);
-                        break;
-                    case '/':
-                        stos.push(y / x);
-                        break;
-                    case '*':
-                        stos.push(y * x);
-                        break;
-                    }
-                }
-            }
-      
-        return stos.top();
-        
-    }
+    int ONP(wxString r);
 };
 
